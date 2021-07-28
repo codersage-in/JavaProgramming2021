7c21aabafd8705e20b6312859d3de9975d8ff824
20IT121 <20IT121@charusat.edu.in>
2021-07-28 14:21:59 +0530

    Uploaded SumOfDigits

---
+++ src/SumOfDigits.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class SumOfDigits {
+    public static void main(String ar[]){
+        //Get the input from the user
+        Scanner input=new Scanner(System.in);//creating an object of scanner class
+        //lets call the method of scanner
+        int noOfInputs=input.nextInt();//calling the method of the java class3
+        for(int cnt=0;cnt<noOfInputs;cnt++){
+            String data=input.next();
+            int sum=0;
+            for(int charcnt=0;charcnt<data.length();charcnt++){
+                sum=sum+(data.charAt(charcnt)-'0');
+            }
+            System.out.println(sum);
+        }
+    }
+}
