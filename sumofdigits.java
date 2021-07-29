7ccd9692c223adf0ade1a623b0edba9b52b3ecf9
Vivek-Shah03 <20it138@charusat.edu.in>
2021-07-29 12:35:19 +0530

    uploaded sumofdigits

---
+++ sumofdigits.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class sumofdigits{
+    public static void main(String ar[]){
+        //get input from user
+        Scanner input = new Scanner(System.in);
+        //call the method of scanner
+        int noofinput = input.nextInt();
+
+        for(int counter=0;counter<noofinput;counter++)
+        {
+            int data=input.nextInt();
+            int sum=0;
+            while(data != 0)
+            {
+                sum += (data%10);
+                data /= 10;
+            }
+            System.out.println(sum);
+        }
+
+        }
+
+    }
