b414bc7681b3440b3d612c009a969f9d9e073859
dhruvil savaliya <20it124@charusat.edu.in>
2021-07-29 09:17:59 +0530

    uploaded sumofdigits

---
+++ sumofdigits.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class sumofdigits {
+
+        public static void main(String ar[]){
+
+            Scanner input= new Scanner(System.in);
+            int noOfInput= input.nextInt();
+
+            for(int count=0;count<noOfInput;count++){
+                String data=input.next();
+                int sum=0;
+                for(int CharCount=0;CharCount<data.length();CharCount++){
+                    sum=sum+(data.charAt(CharCount)-'0');
+                }
+                System.out.println(sum);
+
+            }
+        }
+    }
+
+
+
