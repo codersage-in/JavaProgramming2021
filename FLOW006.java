bb4c2ad6d755a88ed5fef39383e66e2397fc9898
PatelHarsh26 <88429686+PatelHarsh26@users.noreply.github.com>
2021-08-04 16:20:51 +0530

    Add files via upload

---
+++ FLOW006.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner input = new Scanner(System.in);
+		int noofinputs = input.nextInt();
+		
+		for(int i = 0;i<noofinputs;i++){
+		    String number = input.next();
+		    int sum=0;
+		    
+		for(int j=0;j<number.length();j++){
+		    sum = sum + (number.charAt(j) - '0');
+		}    
+		System.out.println(sum);
+		}
+	
+	}
+}
