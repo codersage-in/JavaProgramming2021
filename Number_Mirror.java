95aeffd4b026a61e037bf048876741c71913c868
Dharmendra Prajapati <20IT117@charusat.edu.in>
2021-08-04 21:18:44 +0530

    Uploaded First_And_Last_Digit

---
+++ Number_Mirror.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Number_Mirror {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        System.out.println(n);
+    }
+}
