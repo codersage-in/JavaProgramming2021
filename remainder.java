491c94f8131899c0fb9890803e2b7bfc91e890ca
Hutishseju <sejuhutish@gmail.com>
2021-08-04 13:01:06 +0530

    uploaded remainder.java

---
+++ remainder.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class remainder
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=sc.nextInt();
+        int rem[]=new int [n];
+        for(int i=0; i<n; i++)
+        {
+            int n1=sc.nextInt();
+            int n2=sc.nextInt();
+            rem[i]=n1%n2;
+        }
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(rem[i]);
+        }
+    }
+}
