7322019bdf70ba915d8deb11b09c2da246262863
20it110 <88429803+20it110@users.noreply.github.com>
2021-08-04 21:26:38 +0530

    i have uploaded

---
+++ find remainder.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        for (int i=0;i<n;i++)
+        {
+            int t= sc.nextInt();
+            int p= sc.nextInt();
+            int a=t%p;
+            System.out.println(a);
+        }
+
+    }
+}
