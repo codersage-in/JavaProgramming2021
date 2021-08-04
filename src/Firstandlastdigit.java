f09226006cac4ecb6ca0b51c25b83c500e059756
shailshah2757 <20it135@charusat.edu.in>
2021-08-04 13:21:22 +0530

    uploaded Firstandlastdigit.java

--- src/Firstandlastdigit.java
+++ src/Firstandlastdigit.java
@@ -3,7 +3,7 @@ public class Firstandlastdigit{
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
         int number = scan.nextInt();
-        System.out.println("Given number is: "+ number);
+        System.out.println("Number is: "+ number);
         int firstdigit = 0;
         int lastdigit = 0;
         lastdigit = number % 10;
