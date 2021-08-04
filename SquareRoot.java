8c8c8e5418cebc822e757ea910daa56c38fffa59
20IT121 <88077547+20IT121@users.noreply.github.com>
2021-08-04 16:49:17 +0530

    Add files via upload
    Squareroot.java Uploaded

---
+++ SquareRoot.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class SquareRoot {
+    public static void main(String[] arg){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(Math.sqrt(n));
+    }  
+}
