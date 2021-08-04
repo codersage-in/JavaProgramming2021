3c8dac379336bbc2be2bbc31c152feae891b332c
ompatel121 <80377144+ompatel121@users.noreply.github.com>
2021-08-04 22:41:01 +0530

    Add files via upload

---
+++ reverseofnumber.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+    int num=0;
+      int reversenum =0;
+      Scanner in = new Scanner(System.in);
+      num = in.nextInt();
+      while( num != 0 )
+      {
+          reversenum = reversenum * 10;
+          reversenum = reversenum + num%10;
+          num = num/10;
+      }
+
+      System.out.println("Reverse of input number is: "+reversenum);
+   }
+}
+
