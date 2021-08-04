2b6e05a5dc0d5a0050de22d509a9995bcf8b2215
20it100 <88429424+20it100@users.noreply.github.com>
2021-08-04 23:11:17 +0530

    Add files via upload

---
+++ remainder.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t =sc.nextInt();
+		int a;
+		int b;
+		for(int i=0;i<t;i++) {
+			a=sc.nextInt();
+			b=sc.nextInt();
+			int r=a%b;
+			System.out.println(r);
+			r=0;
+		}
+		
+
+	}
+
+}
