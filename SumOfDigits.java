c8528eb421e984160ea2633eb0d357cf11ec1cb3
Bhavan0925 <88429834+Bhavan0925@users.noreply.github.com>
2021-08-04 23:03:47 +0530

    Update SumOfDigits.java

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -7,23 +7,19 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
-	public static void main (String[] args) throws java.lang.Exception
-	{
-    	// Take Input from user
-    	Scanner input = new Scanner(System.in);
-    	int noOfInputs = input.nextInt();
-    	
-    	for(int i =0;i<noOfInputs;i++){
-    	    String number = input.next(); // 135 (1 + 3 + 5)
-    	    int sum = 0;
-    	// how to get individual digits from the Input
-    	    for(int j = 0; j<number.length(); j++){
-    	        // Do addition of the digits
-    	        sum = sum + (number.charAt(j) - '0'); // char 1 corresponing ascii value
-    	    }
-    	System.out.println(sum);
-    	
-    	}
-    	
-	}
+    public static void main (String[]args) throws java.lang.Exception
+    {
+        Scanner input = new Scanner(System.in);
+        int noOfInputs = input.nextInt();
+        for(int i=0;i<noOfInputs;i++){
+             String number = input.next();
+             int sum=0;
+             for(int j=0;j<number.length(); j++){
+                 sum = sum + (Number.charAT - '0');
+                 
+             }
+             system.out.println(sum);
+           }
+        }
+        
 }
