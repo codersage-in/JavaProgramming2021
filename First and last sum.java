17570b9505675f320ec6f7c463886cfa54ae3768
prit67 <88429840+prit67@users.noreply.github.com>
2021-08-04 16:26:22 +0530

    Add files via upload
    i uploaded 2nd programm.

---
+++ First and last sum.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		String[] num=new String[n];
+		for(int i=0;i<n;i++)
+		{
+		    num[i]=sc.next();
+		}
+		for(int j=0;j<n;j++)
+		{
+		    int sum=0;
+		    int first=num[j].charAt(0)-'0';
+		    int last=num[j].charAt(num[j].length()-1)-'0';
+		    sum=first+last;
+		    System.out.println(sum);
+		}
+	}
+}
