55da204e0072d93dc08830bf8f081846a9a52303
Patelhens5012 <88429421+Patelhens5012@users.noreply.github.com>
2021-08-04 16:24:10 +0530

    remainder

---
+++ REmainder.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+
+public static void main(String[] args) throws IOException{
+ // TODO Auto-generated method stub
+ Scanner re = new Scanner(System.in);
+ int t = re.nextInt();
+ for(int i=0 ; i<t ; i++)
+ {
+ int a = re.nextInt();
+ int b = re.nextInt();
+ 
+ System.out.println(a%b);
+ 
+ }
+ }
+ 
+}
