1a55ed592a5968166846702bd46af9f692a263b4
20IT121 <88077547+20IT121@users.noreply.github.com>
2021-08-04 13:52:19 +0530

    Add files via upload
    Uploaded Remainder.java

---
+++ Remainder.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Remainder {
+    public static void main(String[] arg){
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        int den = in.nextInt();
+        System.out.println(num%den);
+    }
+}
