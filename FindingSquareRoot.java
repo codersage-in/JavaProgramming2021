a00690ec3a29e2f1d6b6b6ecaed38e3ea58465bf
Prince2903 <88429513+Prince2903@users.noreply.github.com>
2021-08-04 21:47:44 +0530

    FindingSquareRoot

---
+++ FindingSquareRoot.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+class SquareRoot
+{
+	public static void main (String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+
+		// Get the total nos
+           int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) 
+	  {
+            int k = sc.nextInt();
+            int res = (int)Math.sqrt(k);
+            System.out.println(res);
+        }
+
+	}
+}
