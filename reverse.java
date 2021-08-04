0803e240a4ff0b1150c4dd878f43a1fe7f66a8fc
Patelhens5012 <88429421+Patelhens5012@users.noreply.github.com>
2021-08-04 21:44:57 +0530

    Reverse

---
+++ reverse.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+ class ReverseTheNumber {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Get the total nos.
+        int loop = sc.nextInt();
+
+        sc.nextLine();
+
+        // Declare the varaible to hold the numbers
+        int data = 0;
+
+        // Get the individual numbers
+        for (int i = 0; i < loop; i++) {
+            data = sc.nextInt();
+            int rev=0, rem=0;
+            while(data >0){
+                // Get the last digit
+                rem = data % 10;
+                // put it at front
+                rev = (rev * 10) + rem;
+                // Remove the last digit
+                data = data /10 ;
+            }
+            System.out.println(rev);
+        }
+
+    }
+}
