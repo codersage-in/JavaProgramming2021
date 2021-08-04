4e1c8330c42dc0f7bd2de795c5ddc44766d1784a
Het9979 <80376658+Het9979@users.noreply.github.com>
2021-08-04 16:21:42 +0530

    Add files via upload

---
+++ Sum of digits.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc= new Scanner(System.in);
+		int t=sc.nextInt();
+		int n,sum=0;
+		for (int i=0;i<t;i++){
+		    n=sc.nextInt();
+		    while(n>0){
+		    sum=sum+(n%10);
+		    n/=10;
+		    }
+		    System.out.println(sum);
+		    sum=0;
+		}
+		
+		
+	}
+}
