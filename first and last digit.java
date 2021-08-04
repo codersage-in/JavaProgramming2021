b668bb864c129d4e5c141b527c93210e71d4dafc
khush2407 <88429450+khush2407@users.noreply.github.com>
2021-08-04 16:26:57 +0530

    Add files via upload
    first and last digit

---
+++ first and last digit.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        while(T-->0){
+            int n=sc.nextInt();
+            int [] a=new int[100];
+            int i=0;
+            while(n!=0){
+                a[i]=n%10;
+                n=n/10;
+                i++;
+            }
+            int sum=a[0]+a[i-1];
+        
+            System.out.println(sum);
+        }
+	}
+}
