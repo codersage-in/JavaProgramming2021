eb15930e7717e1a81e9b4d546f9b86cb00696899
purv2219 <88077869+purv2219@users.noreply.github.com>
2021-08-04 13:56:00 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+public class sum{
+    public static void main(String ar[]){
+
+        Scanner input= new Scanner(System.in);
+        int noOfInput= input.nextInt();
+
+        for(int count=0;count<noOfInput;count++){
+            String data=input.next();
+            int sum=0;
+            for(int CharCount=0;CharCount<data.length();CharCount++){
+                sum=sum+(data.charAt(CharCount)-'0');
+            }
+            System.out.println(sum);
+
+        }
+    }
+}
