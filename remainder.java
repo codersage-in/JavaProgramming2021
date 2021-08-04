c7c6d3ea279df7903090803ecccef9a608bb1f89
20it124 <88083663+20it124@users.noreply.github.com>
2021-08-04 15:03:23 +0530

    Add files via upload

---
+++ remainder.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class remainder {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        int num1,num2;
+        for(int i=0;i<n;i++){
+            num1=sc.nextInt();
+            num2=sc.nextInt();
+            int rem=num1%num2;
+            System.out.println(rem);
+        }
+
+    }
+
+}
