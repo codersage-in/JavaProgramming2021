66d27806e7dd7c7c446f7772a2fe3a5c36ab7e84
20it110 <88429803+20it110@users.noreply.github.com>
2021-08-04 16:24:51 +0530

    I have completed

---
+++ sum of digits.java
@@ -0,0 +1,25 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+                int n=sc.nextInt();
+        int sum = 0;
+               for (int i=0;i<n;i++){
+                    int t= sc.nextInt();
+                    while(t>0) {
+                        sum = sum + (t % 10);
+                        t = t / 10;
+                    }
+                   System.out.println(sum);
+                    sum=0;
+                }
+	}
+}
