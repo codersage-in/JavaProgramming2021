f489f34f8ba0b34e262f18b92bcae537df54b521
pratham120 <88429570+pratham120@users.noreply.github.com>
2021-08-04 16:22:30 +0530

    uploaded code of sumofdigits

---
+++ sumofdigits.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner input = new Scanner(System.in);
+		int noofinput = input.nextInt();
+		
+		for(int i=0;i<noofinput;i++)
+		{
+		    String number = input.next();
+		    int sum = 0;
+		    for(int j=0;j<number.length();j++)
+	        {
+		         sum = sum + (number.charAt(j) - '0'); 
+		    }
+	    	System.out.println(sum);
+		}
+    }   
+}
