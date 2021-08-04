7c3240d9bde2b444e98ac293674482ab3f50ff9a
ShahDevanshi1910 <80448839+ShahDevanshi1910@users.noreply.github.com>
2021-08-04 18:49:22 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class reverse
+{
+    public static void main (String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,number,rev;
+        n=sc.nextInt();
+        if(n>=1 && n<=1000)
+        {
+            for(int i=0;i<n;i++)
+            {
+                number=sc.nextInt();
+                rev=0;
+                if(number>=1 && number<=1000000)
+                {
+                    while(number>0)
+                    {
+                        int rem=number%10;
+                        rev=rev*10+rem;
+                        number=number/10;
+                    }
+                }
+                System.out.println(rev);
+            }
+        }
+    }
+}
