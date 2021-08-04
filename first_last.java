8ef5d4f8ebe1edab0547442bf16235a329f7b051
20IT120-JIMMY <80442931+20IT120-JIMMY@users.noreply.github.com>
2021-08-05 00:41:50 +0530

    Create first_last.java

---
+++ first_last.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class first_last{
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num;
+        num=sc.nextInt();
+        int number;
+        for(int i=0;i<num;i++){
+            int cnt=0,sum=0;
+            number=sc.nextInt();
+            int numbercopy=number;
+            int last=number%10;
+            while(number>0){
+                int r=number%10;
+                cnt++;
+                number=number/10;
+            }
+            int first=(int)(numbercopy/Math.pow(10,cnt-1));
+            sum=sum+first+last;
+            System.out.println(sum);
+        }
+    }
+    
+}
