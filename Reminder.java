a902ec74126a4daa24428444c366894e7359f6c6
Prince2903 <88429513+Prince2903@users.noreply.github.com>
2021-08-04 21:06:07 +0530

    i upload a program to find the reminder

---
+++ Reminder.java
@@ -0,0 +1,20 @@
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        //int num1,num2;
+        for(int i=0;i<n;i++){
+            int num1=sc.nextInt();
+            int num2=sc.nextInt();
+            int rem=num1%num2;
+            System.out.println(rem);
+        }
+
+    }
+    
+}
