b613f87b5c9024bc37c68b76c75d5074b1542592
20it114 <88077784+20it114@users.noreply.github.com>
2021-08-05 12:28:18 +0530

    Add files via upload

---
+++ Second_Largest.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Second_Largest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter total number of test case that you want=");
+        int t = sc.nextInt();
+
+        int[] num;
+        num = new int[3];
+
+        for(int i=0;i<t;i++) {
+            System.out.print("Enter a value of A=");
+            num[0] = sc.nextInt();
+            System.out.print("Enter a value of B=");
+            num[1] = sc.nextInt();
+            System.out.print("Enter a value of C=");
+            num[2] = sc.nextInt();
+            if (num[0] < num[1] && num[0] > num[2])
+            {
+                System.out.println("Secontd largest is="+num[0]);
+            }
+            else if(num[1] < num[2] && num[1] > num[0])
+            {
+                System.out.println("Secontd largest is="+num[1]);
+            }
+            else
+            {
+                System.out.println("Secontd largest is="+num[2]);
+            }
+        }
+
+    }
+}
