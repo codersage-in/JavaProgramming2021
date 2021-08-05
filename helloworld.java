8b4dcf0de1a68603001f5a18edf2412cb3ddf373
ShahDevanshi1910 <80448839+ShahDevanshi1910@users.noreply.github.com>
2021-08-05 11:22:18 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class helloworld
+{
+    public static void main(String[] args)
+    {
+        Scanner S = new Scanner(System.in);
+        System.out.println("Hello World!!");
+    }
+}
