f351a6bae1a697d298ff7f6c72d45f5d7803099c
20it114 <88077784+20it114@users.noreply.github.com>
2021-08-05 12:39:53 +0530

    Add files via upload

---
+++ Cup_Cake.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Cup_Cake {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int i;
+        for (i = 0; i < t; i++) {
+            int a = sc.nextInt();
+            System.out.println((a / 2) + 1);
+        }
+    }
+}
