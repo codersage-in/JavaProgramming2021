5b0bea8ca7808c1e20d6c47ec82061aa87f3fb42
20it114 <88077784+20it114@users.noreply.github.com>
2021-08-05 12:48:16 +0530

    Add files via upload

---
+++ HelpingChef.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class HelpingChef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter testcase =");
+        int t= sc.nextInt();
+        while(t>0)
+        {
+            for (int i = 0; i < t; i++) {
+                int n = sc.nextInt();
+                if (n <= 10) {
+                    System.out.println("Thanks for helping Chef!");
+                } else {
+                    System.out.println("-1");
+                }
+            }
+        }
+
+    }
+}
