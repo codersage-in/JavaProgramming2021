cbffbba11f371f02d83fb76c9a86f1a26557897e
20it124 <88083663+20it124@users.noreply.github.com>
2021-08-05 13:28:04 +0530

    Add files via upload

---
+++ packgingcupcakes.java
@@ -0,0 +1,21 @@
+//Explain Scanner class for taking input
+//Explain for loop
+//Explain  division (/) operator
+
+
+import java.util.Scanner;
+
+class PackagingCupcakes {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Get the total nos
+        int t = input.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int a = input.nextInt();
+            System.out.println((a / 2) + 1);
+        }
+
+    }
+}
