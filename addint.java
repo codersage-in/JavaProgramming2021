fe19731e658faaea646b5ca00f4dabdd1f011759
ShahDevanshi1910 <80448839+ShahDevanshi1910@users.noreply.github.com>
2021-08-05 13:44:26 +0530

    Add files via upload

---
+++ addint.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class addint {
+    public static void main(String[] args)
+    {
+        Scanner S = new Scanner(System.in);
+        System.out.println("Enter Two Integers");
+        int first = 5;
+        int second = 5;
+        System.out.println(first + "+" + second);
+        int sum = first + second;
+        System.out.println("Sum=" + sum);
+    }
+}
