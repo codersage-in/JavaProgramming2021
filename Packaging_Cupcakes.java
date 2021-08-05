7ca34b2416425369bdb45acc2333550681e3dcc1
20it138 <80442960+Vivek-Shah03@users.noreply.github.com>
2021-08-05 13:49:48 +0530

    Add files via upload

---
+++ Packaging_Cupcakes.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Packaging_Cupcakes {
+    public static void main(String arr[])
+    {
+        Scanner s = new Scanner(System.in);
+        int t=s.nextInt();
+        for(;t>0;t--)
+        {
+            int a=s.nextInt();
+            System.out.println((a/2)+1);
+        }
+    }
+}
