e5900a53f2e2b2ee7601c6f150bdc7b1a259c411
20IT130 <88077549+20IT130@users.noreply.github.com>
2021-08-05 12:14:02 +0530

    Add files via upload

---
+++ HELLOWORLD.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HELLOWORLD {
+
+    public static void main(String[] args) {
+
+
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Hello World!!!!!!!!!!!");
+
+        
+    }
+}
