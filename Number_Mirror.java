06f5db96371396148e7cb585ac5a5344634d6382
ShahDevanshi1910 <80448839+ShahDevanshi1910@users.noreply.github.com>
2021-08-06 17:58:25 +0530

    Add files via upload

---
+++ Number_Mirror.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Number_mirror
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner S = new Scanner(System.in);
+        System.out.println("num=");
+        int num = S.nextInt();
+        System.out.println("num=" + num);
+    }
+}
