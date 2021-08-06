4149acc1872b4c0a58f6dded51e7484e151450f5
20IT130 <88077549+20IT130@users.noreply.github.com>
2021-08-06 17:40:46 +0530

    Add files via upload

---
+++ Mathematical_Operation.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Mathematical_Operation {
+    public static void main(String args[])
+    {
+        Scanner Operations=new Scanner(System.in);
+        System.out.printf("Enter the number a:");
+        int a=Operations.nextInt();
+        System.out.printf("Enter the number b:");
+        int b=Operations.nextInt();
+        int Addition=a+b;
+        System.out.printf("Addition Answer is : "+Addition);
+        System.out.printf("          ");
+        int Substraction=a-b;
+        System.out.printf("Substraction Answer is : "+Substraction);
+        System.out.printf("          ");
+        int Multiplication=a*b;
+        System.out.printf("Multiplication Answer is : "+Multiplication);
+        System.out.printf("          ");
+        int Division=a/b;
+        System.out.printf("Division Answer is : "+Division);
+
+
+
+
+
+
+
+
+    }
+}
