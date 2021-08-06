098fd5b41f7e62ed5f59c179e7c07e9e33451c4e
Dharmendra Prajapati <20IT117@charusat.edu.in>
2021-08-06 08:47:21 +0530

    Uploaded Square_Roots File

---
+++ Square_Roots.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Square_Roots {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+        for(int i=0;i<N;i++){
+            int num = scan.nextInt();
+            System.out.println((int)Math.sqrt(num));
+        }
+    }
+}
