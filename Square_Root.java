c1fcf4ba8aa35f9fcf1c7c9069309e70880e3087
20IT130 <88077549+20IT130@users.noreply.github.com>
2021-08-06 18:59:42 +0530

    Add files via upload

---
+++ Square_Root.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Square_Root {
+    public static void main(String arg[]){
+        Scanner N=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num=N.nextInt();
+        System.out.println((int)Math.sqrt(num));
+
+
+    }
+}
