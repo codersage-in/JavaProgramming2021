8ffab820275cc2944f098d9c400cfb0022be2b1e
rudrapatel1308 <88429498+rudrapatel1308@users.noreply.github.com>
2021-08-11 15:10:59 +0530

    Add files via upload

---
+++ javaloop1.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int multiplier = scan.nextInt();
+        scan.close();
+        for (int i = 1; i <= 10; i++) {
+            System.out.format("%d x %d = %d%n", multiplier, i , i * multiplier);
+        }
+    }
+}
