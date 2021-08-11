1bb7b12da02b1ebde7a34c2d7b284a39b08622f8
20IT127 <82073450+Hutishseju@users.noreply.github.com>
2021-08-11 13:48:10 +0530

    Add files via upload

---
+++ stringintro.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class stringintro 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        System.out.println(A.length() + B.length());
+        if(A.compareTo(B)>0)
+        {
+            System.out.println("YES");
+        }
+        else
+        {System.out.println("NO");}
+        System.out.println( A.substring(0,1).toUpperCase()+A.substring(1,A.length())+" "+B.substring(0,1).toUpperCase()+B.substring(1,B.length()));
+        sc.close();
+    }
+}
