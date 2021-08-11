5457b1b3128f0e66f94dc8fecf2787594b081469
purv2219 <88077869+purv2219@users.noreply.github.com>
2021-08-11 22:42:50 +0530

    Create JavaOutputFormatting.java

---
+++ JavaOutputFormatting.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Solution {
+
+public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++)
+        {
+            String s1=sc.next();
+            int x=sc.nextInt();
+            for (int j=s1.length();j<15;j++){
+                s1 += " ";
+            }
+            System.out.println(s1 + String.format("%03d", x));
+        }
+        System.out.println("================================");
+
+}
+}       
+        
+        
+
+
+
