39b291bedf1d8bd8eac6dbda29cbb9041e9c78dd
20it114 <88077784+20it114@users.noreply.github.com>
2021-08-12 13:46:15 +0530

    Add files via upload

---
+++ JavaStringTokens.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class JavaStringTokens {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s=scan.nextLine();
+        //Complete the code
+        s = s.trim();
+        if (s.length()>400000){
+            return ;
+        }else if (s.length()==0){
+            System.out.println(0);
+            return ;
+        }else{
+            String[] words = s.split("[!,?.*_'@\\ ]+");
+            int count = words.length;
+            System.out.println(count);
+            for(String word : words){
+                System.out.println(word);
+            }
+        }
+    }
+}
+
