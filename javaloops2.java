4d71c8e12e8de8556c0a99861861e0b5ea6ad234
20it133 <20it133@charusat.edu.in>
2021-08-13 15:40:34 +0530

    uploaded

---
+++ javaloops2.java
@@ -0,0 +1,38 @@
+package com.company;
+import java.util.Scanner;
+public class javaloops2
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int q, a, b, n, sum, sum1, mul;
+        q = sc.nextInt();
+        if (q >= 0 && q <= 500) {
+            for (int i = 0; i < q; i++) {
+                a = sc.nextInt();
+                b = sc.nextInt();
+                n = sc.nextInt();
+
+                if (a >= 0 && b >= 0 && a <= 50 && b <= 50 && n >= 1 && n <= 15) {
+                    for (int j = 0; j < n; j++) {
+                        sum1 = 0;
+                        sum1 = sum1 + a;
+                        sum = 0;
+
+                        for (int k = 0; k <= j; k++) {
+
+                            mul = 1;
+                            mul = mul * b * (int) (Math.pow(2, k));
+                            //System.out.print(mul+" ");
+                            sum = sum + mul;
+                            //System.out.print(sum+" ");
+                        }
+
+                        System.out.print(sum + a + " ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+}
