36768d6d47432c29fc554a5489fa9cda5a37cefc
20it133 <20it133@charusat.edu.in>
2021-08-13 15:02:08 +0530

    uploaded

---
+++ welcometojava.java
@@ -0,0 +1,7 @@
+package com.company;
+public class welcometojava {
+    public static void main(String args[]) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
