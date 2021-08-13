c6ed255331232271bacb60f308ca296bd2e6f08f
20it114 <88077784+20it114@users.noreply.github.com>
2021-08-14 00:19:49 +0530

    Add files via upload

---
+++ CielAndReceipt.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class CielAndReceipt {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int n = sc.nextInt();
+            int max=2048;
+            int count=0;
+            while(n>0)
+            {
+                count=count+n/max;
+                n=n%max;
+                max=max/2;
+            }
+            System.out.println(count);
+        }
+    }
+}
