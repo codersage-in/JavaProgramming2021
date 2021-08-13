4b946fc5e302d892b66f3d41cb82834748b5867a
20it133 <20it133@charusat.edu.in>
2021-08-13 15:47:11 +0530

    uploaded

---
+++ javaendoftile.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+public class javaendoftile
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        for (int i = 1; scan.hasNext(); i++)
+        {
+            System.out.println(i + " " + scan.nextLine());
+        }
+    }
+}
