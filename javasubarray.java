cf204bb1d649910030cbc27887ae8fae8cf34532
20it133 <20it133@charusat.edu.in>
2021-08-18 20:05:51 +0530

    uploaded

---
+++ javasubarray.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class javasubarray
+{
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int count =0;
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++)
+        {
+            arr[i] = in.nextInt();
+        }
+        for(int i=0; i<n; i++)
+        {
+            int sum = 0;
+            for (int j=i; j<n; j++)
+            {
+                sum = arr[j]+sum;
+                if (sum<0)
+                {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
