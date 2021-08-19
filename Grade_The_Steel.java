ce6b2907e9f94719229c5b3b755ebe18469d096b
Dharmendra Prajapati <82072182+Dharmendra8276@users.noreply.github.com>
2021-08-19 08:51:57 +0530

    Add files via upload

---
+++ Grade_The_Steel.java
@@ -0,0 +1,35 @@
+package JavaProgramming2021;
+
+import java.util.Scanner;
+
+public class Grade_The_Steel {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+
+        for(int i=0;i<N;i++){
+            int hard = scan.nextInt();
+            float carbon = scan.nextFloat();
+            int tensile = scan.nextInt();
+
+            if(hard>50 && carbon<0.7 && tensile>5600){
+                System.out.println("10");
+            }
+            else if(hard>50 && carbon<0.7){
+                System.out.println("9");
+            }
+            else if(carbon<0.7 && tensile>5600){
+                System.out.println("8");
+            }
+            else if(hard>50 && tensile>5600){
+                System.out.println("7");
+            }
+            else if(hard>50 || tensile>5600 ||carbon<0.7){
+                System.out.println("6");
+            }
+            else{
+                System.out.println("5");
+            }
+        }
+    }
+}
