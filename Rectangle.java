2d9781702afa66133fa535edd3229b84f4a1cbef
ompatel121 <80377144+ompatel121@users.noreply.github.com>
2021-09-04 15:40:50 +0530

    Add files via upload
    Here I am uploading a code of Rectangle

--- Rectangle.java
+++ Rectangle.java
@@ -1,14 +1,28 @@
-import java.util.Scanner;
-public class Rectangle {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int count = sc.nextInt();
-        for (int j = 1; j <= count; j++) {
-            int a=sc.nextInt();
-            int b=sc.nextInt();
-            int c=sc.nextInt();
-            int d= sc.nextInt();
-            if((a==b&&c==d)||(a==c&&b==d)||(a==d&&c==b)||(b==c&&a==d)||(b==d&&a==c)||(c==d&&a==b)){
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+         Scanner input=new Scanner(System.in);
+        int T=input.nextInt();
+        int a[]=new int[T];
+        int b[]=new int[T];
+        int c[]=new int[T];
+        int d[]=new int[T];
+        for(int i=0;i<T;i++){
+            a[i]=input.nextInt();
+            b[i]=input.nextInt();
+            c[i]=input.nextInt();
+            d[i]=input.nextInt();
+            if(a[i]+b[i]==c[i]+d[i]){
+                System.out.println("YES");
+            }
+            else if(a[i]+c[i]==b[i]+d[i]){
+                System.out.println("YES");
+            }
+            else if(a[i]+d[i]==b[i]+c[i]){
                 System.out.println("YES");
             }
             else{
