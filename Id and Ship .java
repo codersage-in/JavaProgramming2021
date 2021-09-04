f9254a0d388f6091658949ffe1f5206e2f589b89
20it085 <88103864+20it085@users.noreply.github.com>
2021-09-04 15:40:23 +0530

    Add files via upload

---
+++ Id and Ship .java
@@ -0,0 +1,29 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            String  S = sc.next();
+            if(S.equals("B")|| S.equals("b")){
+                System.out.println("BattleShip");
+            }else if(S.equals("C")||S.equals("c")){
+                System.out.println("Cruiser");
+            }else if(S.equals("D")|| S.equals("d")){
+                System.out.println("Destroyer");
+            }else if(S.equals("F")|| S.equals("f")){
+                System.out.println("Frigate");
+            }
+        }
+	}
+}
