b134c39d9e93bfc65b7686631c7a5e6359ff7145
shail20IT136 <88127142+shail1012@users.noreply.github.com>
2021-09-04 20:35:04 +0530

    Add files via upload

--- smallfactorial.java
+++ smallfactorial.java
@@ -1,22 +1,18 @@
 import java.util.*;
-public class smallfactorial
+import java.lang.*;
+import java.io.*;
+class Codechef
 {
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        int t,n,x=1;
-        System.out.println("Enter the length");
-        t=sc.nextInt();
-        while(t--!=0)
-        {
-            System.out.println("Enter the number = ");
-            n=sc.nextInt();
-            for(int i=n;i>0;i--)
-            {
-                x = x * i;
-            }
-            System.out.println("Factorial = " + x);
-            x=1;
-        }
-    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+		Scanner sc = new Scanner(System.in);
+	    int	n=sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0;i<n;i++)
+		arr[i]=sc.nextInt();
+		Arrays.sort(arr);
+		for(int i=0;i<n;i++)
+		System.out.println(arr[i]);
+	}
 }
