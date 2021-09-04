e94e8200f9f875c86e459eebfb8f04479c44e013
ShahDevanshi1910 <80448839+ShahDevanshi1910@users.noreply.github.com>
2021-09-04 17:07:25 +0530

    Add files via upload

---
+++ Gross salary.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner sc=new Scanner(System.in);
+	  int a=sc.nextInt();
+	  for(int i=0;i<a;i++)
+	  {
+	      float b=sc.nextInt();
+	      float HRA =0;
+	      float DA=0;
+	     // float GS =0;
+	      if(b<1500)
+	      {
+	          HRA = (10*b)/100;
+	          DA = (90*b)/100;
+	      }
+	      else
+	      {
+	      if(b>=1500)
+	       {
+	          HRA = 500;
+	          DA = (98*b)/100;
+	      }
+	      
+	      }
+	      System.out.println(b+HRA+DA);
+	  }
+	}
+}
