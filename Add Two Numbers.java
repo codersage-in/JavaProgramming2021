9ab5cde9ceb12da3477b7f127ca60c540e898ec1
20it085 <88103864+20it085@users.noreply.github.com>
2021-09-04 15:02:30 +0530

    Add files via upload

---
+++ Add Two Numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class A{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        int sum=0;
+        for(int i=0;i<t;i++){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            sum=a+b;
+            System.out.println(sum);
+        }
+    }
+}
