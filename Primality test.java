d4bf37a378f71c6ee7f002c8b975ae7193fa5c33
ShahDevanshi1910 <80448839+ShahDevanshi1910@users.noreply.github.com>
2021-09-04 17:02:15 +0530

    Add files via upload

---
+++ Primality test.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int a=sc.nextInt();
+		    int c=0;
+		    for(int j=1;j<=a;j++)
+		    {
+		        if(a%j==0)
+		        c++;
+		        
+		    }
+		    if(c==2)
+		    System.out.println("yes");
+		    else System.out.println("no");
+		}
+	}
+}
