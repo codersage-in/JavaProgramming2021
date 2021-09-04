ae89919f5f03261bc739de188838aa44c0ab0163
20it135 <80442908+shailshah2757@users.noreply.github.com>
2021-09-04 17:09:52 +0530

    Add files via upload

---
+++ Gross salary.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner sc=new Scanner(System.in);
+	  int a=sc.nextInt();
+	  for(int i=0;i<a;i++)
+	  {
+	      float b=sc.nextInt();
+	      float HRA =0;
+	      float DA=0;
+	     
+	      if(b<1500)
+	      {
+	          HRA = (10*b)/100;
+	          DA = (90*b)/100;
+	      }
+	      else
+	      {
+	      if(b>=1500)
+	       {
+	          HRA = 500;
+	          DA = (98*b)/100;
+	      }
+	      
+	      }
+	      System.out.println(b+HRA+DA);
+	  }
+	 
+	}
+	
+}
