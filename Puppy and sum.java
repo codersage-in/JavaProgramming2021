e2a31b9b3f5ede76bd1350de901235040a56f68f
20it135 <80442908+shailshah2757@users.noreply.github.com>
2021-09-04 16:49:50 +0530

    Add files via upload

---
+++ Puppy and sum.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		   	int a=sc.nextInt();
+		   	int b=sc.nextInt();
+		   //	int sum=0;
+		   	for(int j=1;j<=a;j++)
+		   	{
+		   	    int sum=0;
+		   	     while(b!=0)
+                {
+                    sum+=b;
+                    b--;
+                }
+               b=sum;
+               
+		   	}
+            
+            System.out.println(b);
+		}
+		
+	    
+	}
+}
