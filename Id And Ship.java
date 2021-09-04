67428ae18c05d7bba9fd4fbf8d6cbac1c9a8c63a
20IT130 <88077549+20IT130@users.noreply.github.com>
2021-09-04 16:26:23 +0530

    Add files via upload

---
+++ Id And Ship.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		  Scanner input =new Scanner(System.in);
+		 int t=input.nextInt();
+        char a;
+        while(t-- >0)
+        {
+            a=input.next().charAt(0);
+            if(a=='B' || a=='b')
+                System.out.println("BattleShip");
+            else if(a=='C' || a=='c')
+                System.out.println("Cruiser");
+            else if (a=='D' || a=='d')
+                System.out.println("Destroyer");
+            else if(a=='F' || a=='f')
+                System.out.println("Frigate");
+        }
+   
+	}
+}
