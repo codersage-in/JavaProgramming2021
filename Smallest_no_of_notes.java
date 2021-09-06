ae325feb2e6a43ac22a2a83600e148da8707022a
20IT120-JIMMY <80442931+20IT120-JIMMY@users.noreply.github.com>
2021-09-06 11:00:23 +0530

    Create Smallest_no_of_notes.java

---
+++ Smallest_no_of_notes.java
@@ -0,0 +1,25 @@
+mport java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		  Scanner sc=new Scanner(System.in);
+    int t=sc.nextInt();
+    if(t>=1 && t<=1000){
+        for(int i=0;i<t;i++){
+            int sum=0;
+            int n=sc.nextInt();
+            int arr[]={1,2,5,10,50,100};
+            for(int j=5;j>=0;j--){
+                sum+=(int)n/arr[j];
+                n=n%arr[j];
+            }
+            System.out.println(sum);
+        }
+    }
+	}
+}
