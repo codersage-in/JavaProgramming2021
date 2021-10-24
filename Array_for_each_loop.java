215eec3d4557e67e497fcf85a87dc29690eac5b5
ShahDevanshi1910 <80448839+ShahDevanshi1910@users.noreply.github.com>
2021-10-24 22:38:12 +0530

    Add files via upload

---
+++ Array_for_each_loop.java
@@ -0,0 +1,15 @@
+public class Array_for_each_loop
+{
+    public static void main(String[] args)
+    {
+        // create an array
+        int[] age = {12, 4, 5};
+        // loop through the array
+        // using for loop
+        System.out.println("Using for-each Loop:");
+        for(int a : age)
+        {
+            System.out.println(a);
+        }
+    }
+}
