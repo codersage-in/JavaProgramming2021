15dc9162cd8db6d136358bb813ec337dd7b36cd3
ShahDevanshi1910 <80448839+ShahDevanshi1910@users.noreply.github.com>
2021-10-24 18:37:34 +0530

    Add files via upload

---
+++ Java_Input.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Java_Input
+{
+    public static void main(String args[])
+    {
+        Scanner S = new Scanner(System.in);
+        int number = S.nextInt();
+        System.out.println(number);
+        System.out.println(S);
+    }
+}
+
+//Here, number will print the number you print amd S will not give an error but it will print one long statement, or you can say string which will five some information of the through a brief explanation.
