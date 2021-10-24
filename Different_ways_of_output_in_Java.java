b0e3f6b4abb6cf9ab34774ba659181163ea71bfb
ShahDevanshi1910 <80448839+ShahDevanshi1910@users.noreply.github.com>
2021-10-24 18:23:03 +0530

    Add files via upload

---
+++ Different_ways_of_output_in_Java.java
@@ -0,0 +1,12 @@
+public class Different_ways_of_output_in_Java
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Java programming is interesting.");
+        System.out.print("Java programming is interesting.");
+        System.out.printf("Java programming is interesting.");
+    }
+}
+//print() - It prints string inside the quotes.
+//Println() - It prints string inside the quotes similar like print() method. Then the cursor moves to the beginning of the next line.
+//printf() - It provides string formatting.
