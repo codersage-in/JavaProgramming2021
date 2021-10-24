50d5732f4cb013d47e70e4f3803edf55cfb4db08
ShahDevanshi1910 <80448839+ShahDevanshi1910@users.noreply.github.com>
2021-10-24 18:05:57 +0530

    Add files via upload

---
+++ Char_Type.java
@@ -0,0 +1,14 @@
+class Char_Type
+{
+    public static void main(String[] args)
+    {
+
+        char letter = '\u0051';
+        System.out.println(letter);  // prints Q
+        char letter1 = '9';
+        System.out.println(letter1);  // prints 9
+        char letter2 = 65;
+        System.out.println(letter2);  // prints A
+
+    }
+}
