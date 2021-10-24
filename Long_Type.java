b05dae13509d6592a4d208f2d1944ef0c665e1fc
ShahDevanshi1910 <80448839+ShahDevanshi1910@users.noreply.github.com>
2021-10-24 17:50:07 +0530

    Add files via upload

---
+++ Long_Type.java
@@ -0,0 +1,8 @@
+public class Long_Type
+{
+    public static void main(String[] args)
+    {
+        long range = -42332200000L;
+        System.out.println(range);    // prints -42332200000
+    }
+}
