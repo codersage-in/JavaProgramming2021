ea83755cb12f0ae46b2e29d86659d9d2e8bca731
ShahDevanshi1910 <80448839+ShahDevanshi1910@users.noreply.github.com>
2021-10-24 18:29:16 +0530

    Add files via upload

---
+++ Concantenate_String.java
@@ -0,0 +1,10 @@
+public class Concantenate_String
+{
+        public static void main(String[] args)
+        {
+            Double number = 10.5;
+            System.out.println("I am " + "awesome.");
+            System.out.println("number = " + number);
+        }
+}
+
